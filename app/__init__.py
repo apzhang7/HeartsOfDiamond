9382ed2dd8fd186fb488755ec076c8b7e58621fa
Angela Zhang <apzhang7@gmail.com>
2022-03-04 09:18:18 -0500

    added base base of a flask to run

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,16 @@
 # P02: Wordle Mania!™
 # 2022-03-03
 # Time Spent: ??
+
+from flask import Flask 
+from flask import render_template
+
+app = Flask(__name__)  
+
+@app.route("/")
+def hello_world():
+    return "No hablo queso!"
+    
+if __name__ == "__main__": 
+    app.debug = True
+    app.run()
