e7d3d1cf65221364dc3139edad826cb337a4c614
Jonathan Wu <jwu20@stuy.edu>
2022-03-07 18:15:26 -0500

    added two more routes

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,23 @@
 # 2022-03-03
 # Time Spent: ??
 
-from flask import Flask 
+from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)  
+app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "No hablo queso!"
-    
-if __name__ == "__main__": 
+def normalWordle():
+    return render_template("normal.html")
+
+@app.route("/hard-wordle")
+def hardWordle():
+    return render_template("hard.html")
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
