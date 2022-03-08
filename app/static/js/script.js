e3236fa3bf7b1ec729cf509aeb6721eb0cd24e4b
JesseStuy20 <jxie20@stuy.edu>
2022-03-08 09:43:14 -0500

    Added wordle grid with js canvas

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -3,3 +3,27 @@
 // P02: Wordle Mania!™
 // 2022-03-03
 // Time Spent: ??
+
+var c = document.getElementById("playground"); // GET CANVAS
+var ctx = c.getContext('2d');
+var requestID = false;  //init global var for use with animation frames
+
+var drawGrid = () => {
+  ctx.strokeStyle = 'white';
+  for(var j = 0; j<6; j++) {
+    for(var i = 0; i<5; i++) {
+      ctx.beginPath();
+      ctx.moveTo(520+i*65, 50+j*65);
+      ctx.lineTo(580+i*65, 50+j*65);
+      ctx.stroke();
+      ctx.lineTo(580+i*65, 110+j*65);
+      ctx.stroke();
+      ctx.lineTo(520+i*65, 110+j*65);
+      ctx.stroke();
+      ctx.lineTo(520+i*65, 50+j*65);
+      ctx.stroke();
+    }
+  }
+}
+
+drawGrid();
