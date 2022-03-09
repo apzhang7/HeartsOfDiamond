a227c73b648aa70f5d66652bf756e73839e05562
JesseStuy20 <jxie20@stuy.edu>
2022-03-09 09:42:26 -0500

    Added letter typing functionality to js

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -7,7 +7,7 @@
 var c = document.getElementById("playground"); // GET CANVAS
 var ctx = c.getContext('2d');
 var requestID = false;  //init global var for use with animation frames
-
+var letterPosition = [0,0];
 var drawGrid = () => {
   ctx.strokeStyle = 'white';
   for(var k = 0 ; k < 2; k++) {
@@ -28,4 +28,28 @@ var drawGrid = () => {
   }
 }
 
+
+
 drawGrid();
+document.addEventListener("keydown",letter) ;
+
+function letter(e) {
+  key = e.keyCode ;
+  console.log(key);
+  ctx.font = '48px arial';
+  if ((key == 13) || (key >= 65 && key <= 90)) {
+    if (letterPosition[1] == 5) {
+      console.log("full");
+      if (key == 13 && letterPosition[0] != 6) {
+        letterPosition[0]++;
+        letterPosition[1] = 0;
+      }
+    }
+    else {
+      if (key != 13) {
+        ctx.fillText(String.fromCharCode(key),letterPosition[1]*65+19,letterPosition[0]*65+53)
+        letterPosition[1]++ ;
+      }
+    }
+  }
+};
