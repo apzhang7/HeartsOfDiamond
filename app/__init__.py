86886337f5de38230c32b3db2728d018ffa087c0
Jonathan Wu <jwu20@stuy.edu>
2022-03-13 18:32:09 -0400

    pass through the entire wordbank

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,13 @@
 from flask import Flask
 from flask import render_template
 import random
+import html
 
 app = Flask(__name__)
 
 @app.route("/")
 def normalWordle():
-    return render_template("normal.html", wordle = randomWord(), defaultTime = 180)
+    return render_template("normal.html", wordle = randomWord(), defaultTime = 180, words = wordBank())
 
 @app.route("/hard-wordle")
 def hardWordle():
@@ -35,6 +36,14 @@ def randomWord():
         words = file.readlines()
         randomWord = words[random.randint(0,2313)][:-1]
         return randomWord
+
+def wordBank() :
+    with open('static/words.txt') as file:
+        words = file.readlines()
+        wordBank = ""
+        for word in words:
+            wordBank += str(word.strip()) + " "
+        return wordBank[0:len(wordBank)-1]
 
 if __name__ == "__main__":
     app.debug = True
